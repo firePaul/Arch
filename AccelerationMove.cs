bd5f738328fa10eec4c45a030b34a9dc0b27631c
firePaul <75265154+firePaul@users.noreply.github.com>
2021-05-11 17:48:54 +0300

    Add files via upload

---
+++ AccelerationMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    internal sealed class AccelerationMove : MoveTransform
+    {
+        private readonly float _acceleration;
+
+        public AccelerationMove(Rigidbody2D rigidbody2D, float speed, float acceleration) : base(rigidbody2D, speed)
+        {
+            _acceleration = acceleration;
+        }
+
+        public void AddAcceleration()
+        {
+            Speed += _acceleration;
+        }
+
+        public void RemoveAcceleration()
+        {
+            Speed -= _acceleration;
+        }
+    }
+}
